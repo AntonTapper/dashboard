7a9dfc1aaad388f10656bc4447bee741cdc77755
Anton Tapper Dock <at@appsson.se>
2024-01-31 21:50:09 +0100

    added utilites map and functions

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,15 +1,16 @@
 import { Avatar as AntdAvatar, AvatarProps } from "antd"
 
 type Props = AvatarProps & {
-    name: string
+    name?: string
 }
 
 const CustomAvatar = ({ name, style, ...rest }: Props) => {
   return (
     <AntdAvatar
-        alt="AntonTapper"
+        alt={name}
         size="small"
-        style={{backgroundColor: '#87d068', display: 'flex', alignItems: 'center', border: 'none'}}
+        style={{backgroundColor: '#87d068', display: 'flex', alignItems: 'center', border: 'none', ...style}}
+        {...rest}
     >
         {name}
     </AntdAvatar>
